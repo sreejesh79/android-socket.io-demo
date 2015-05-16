46d96126ead062b4fc194d1667167373476b1468
sreejesh pillai <sreejeshpillai@sreejeshs-MacBook-Pro.local>
2015-05-16 10:19:25 +0530

    final server code

--- server/index.js
+++ server/index.js
@@ -5,12 +5,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile('index.html');
 })
 io.on('connection',function(socket){
-    console.log('one user connected : '+socket.id);
+    console.log('one user connected '+socket.id);
+    socket.on('message',function(data){
+        console.log(data);
+        //io.emit('message',{message:data});
+        var sockets = io.sockets.sockets;
+        sockets.forEach(function(sock){
+            if(sock.id != socket.id)
+            {
+                sock.emit('message',data);
+            }
+        })
+    })
+    socket.on('disconnect',function(){
+        console.log('one user disconnected '+socket.id);
+    })
 })
-
 http.listen(3000,function(){
     console.log('server listening on port 3000');
 })
